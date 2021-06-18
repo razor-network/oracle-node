5321c1a93ed48462c9a5ac3fbe4c2bc1fb6766e3
Ashish Mishra <ashish10677@gmail.com>
2021-06-18 17:00:21 +0530

    Remove unnecessary TODO.
    Signed-off-by: Ashish Mishra <ashish10677@gmail.com>

--- utils/random.go
+++ utils/random.go
@@ -8,7 +8,6 @@ import (
 func GetBlockHashes(client *ethclient.Client, address string) ([]byte, error) {
 	randomClient := GetRandomClient(client)
 	callOpts := GetOptions(false, address, "")
-	// TODO: Check NumberOfBlocks value here. Should it be 10?
 	blockHashes, err := randomClient.BlockHashes(&callOpts, uint8(core.NumberOfBlocks))
 	if err != nil {
 		return nil, err
