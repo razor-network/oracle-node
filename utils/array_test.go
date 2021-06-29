66955de780d892bc141697ee5ad3cd2751234927
Yashk767 <yash.kurlekar2017@gmail.com>
2021-06-29 21:25:04 +0530

    Added nil test case

--- utils/array_test.go
+++ utils/array_test.go
@@ -49,6 +49,14 @@ func TestContains(t *testing.T) {
 			},
 			want: true,
 		},
+		{
+			name: "Test 5",
+			args: args{
+				arr: []*big.Int{},
+				val: nil,
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
