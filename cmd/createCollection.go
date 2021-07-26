121932c0685f9f947314057343bfc8b916220328
Ashish Kumar Mishra <ashish10677@gmail.com>
2021-07-26 12:15:44 +0530

    Remove fee from createJob and createCollection. (#73)
    * Remove fee from createJob and createCollection.
    
    Previously, createJob and createCollection functions in smart contract were payable, but since it was removed, sending ether while calling those functions was throwing gas error. This is fixed in this commit.
    
    Signed-off-by: Ashish Mishra <ashish10677@gmail.com>
    
    * Update README.
    
    Signed-off-by: Ashish Mishra <ashish10677@gmail.com>

--- cmd/createCollection.go
+++ cmd/createCollection.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"math/big"
 	"razor/core"
 	"razor/core/types"
 	"razor/utils"
@@ -23,24 +22,17 @@ var createCollectionCmd = &cobra.Command{
 		password := utils.PasswordPrompt()
 
 		name, _ := cmd.Flags().GetString("name")
-		fee, _ := cmd.Flags().GetString("fee")
 		address, _ := cmd.Flags().GetString("address")
 		jobIds, _ := cmd.Flags().GetStringSlice("jobIds")
 		aggregation, _ := cmd.Flags().GetUint32("aggregation")
 
 		client := utils.ConnectToClient(config.Provider)
 
-		feeInBigInt, ok := new(big.Int).SetString(fee, 10)
-		if !ok {
-			log.Fatal("SetString: error")
-		}
-
 		jobIdsInBigInt := utils.ConvertToBigIntArray(jobIds)
 
 		txnOpts := utils.GetTxnOpts(types.TransactionOptions{
 			Client:         client,
 			Password:       password,
-			EtherValue:     feeInBigInt,
 			AccountAddress: address,
 			ChainId:        core.ChainId,
 			GasMultiplier:  config.GasMultiplier,
@@ -61,22 +53,18 @@ func init() {
 
 	var (
 		Name              string
-		Fee               string
 		Account           string
 		JobIds            []string
 		AggregationMethod uint32
 	)
 
 	createCollectionCmd.Flags().StringVarP(&Name, "name", "n", "", "name of the collection")
-	createCollectionCmd.Flags().StringVarP(&Fee, "fee", "f", "0", "fee")
 	createCollectionCmd.Flags().StringVarP(&Account, "address", "", "", "address of the job creator")
 	createCollectionCmd.Flags().StringSliceVarP(&JobIds, "jobIds", "", []string{}, "job ids for the  collection")
 	createCollectionCmd.Flags().Uint32VarP(&AggregationMethod, "aggregation", "", 1, "aggregation method to be used")
 
 	nameErr := createCollectionCmd.MarkFlagRequired("name")
 	utils.CheckError("Name error: ", nameErr)
-	feeErr := createCollectionCmd.MarkFlagRequired("fee")
-	utils.CheckError("Fee error: ", feeErr)
 	addrErr := createCollectionCmd.MarkFlagRequired("address")
 	utils.CheckError("Address Error: ", addrErr)
 	jobIdErr := createCollectionCmd.MarkFlagRequired("jobIds")
