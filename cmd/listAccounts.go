79cd5b23025058368c34e1dd908033365d1da185
Yashk767 <yash.kurlekar2017@gmail.com>
2021-08-10 14:03:32 +0530

    created listAccounts command

---
+++ cmd/listAccounts.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"razor/utils"
+)
+
+//listAccountsCmd represents listAccounts command
+var listAccountsCmd = &cobra.Command{
+	Use:   "listAccounts",
+	Short: "listAccounts command can be used to list all accessible accounts",
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		path := utils.GetDefaultPath()
+		ks := keystore.NewKeyStore(path, keystore.StandardScryptN, keystore.StandardScryptP)
+		allAccounts := ks.Accounts()
+		log.Info("The available accounts are: ")
+		for _, account := range allAccounts {
+			log.Infof("%s\n", account.Address.String())
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listAccountsCmd)
+}
