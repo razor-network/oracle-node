e43b30a0464c538b16549af01a15268f38e1e240
Yashk767 <yash.kurlekar2017@gmail.com>
2021-08-11 13:28:44 +0530

    Added getters for deployed addresses

---
+++ utils/addresses.go
@@ -0,0 +1,38 @@
+package utils
+
+import "razor/core"
+
+func GetStakeManagerAddress() string {
+	addresses := core.AssignAddressesFromJSON()
+	return addresses.StakeManagerAddress
+}
+
+func GetAssetManagerAddress() string {
+	addresses := core.AssignAddressesFromJSON()
+	return addresses.AssetManagerAddress
+}
+
+func GetVoteManagerAddress() string {
+	addresses := core.AssignAddressesFromJSON()
+	return addresses.VoteManagerAddress
+}
+
+func GetRAZORAddress() string {
+	addresses := core.AssignAddressesFromJSON()
+	return addresses.RAZORAddress
+}
+
+func GetRandomAddress() string {
+	addresses := core.AssignAddressesFromJSON()
+	return addresses.RandomClientAddress
+}
+
+func GetBlockManagerAddress() string {
+	addresses := core.AssignAddressesFromJSON()
+	return addresses.BlockManagerAddress
+}
+
+func getParametersAddress() string {
+	addresses := core.AssignAddressesFromJSON()
+	return addresses.ParametersAddress
+}
