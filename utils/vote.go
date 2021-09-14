1976c4142e63d9de3916879947c776875d19adca
Ashish Mishra <ashish10677@gmail.com>
2021-09-14 22:19:46 +0530

    Implement retry mechanism for vote.go.
    Signed-off-by: Ashish Mishra <ashish10677@gmail.com>

--- utils/vote.go
+++ utils/vote.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"math/big"
 	"razor/core"
+	"razor/core/types"
 	"razor/pkg/bindings"
 )
 
@@ -18,8 +19,19 @@ func GetCommitments(client *ethclient.Client, address string) ([32]byte, error) 
 	if err != nil {
 		return [32]byte{}, err
 	}
-	commitments, err := voteManager.Commitments(&callOpts, stakerId)
-	if err != nil {
+	var (
+		commitments   types.Commitment
+		commitmentErr error
+	)
+	for retry := 1; retry <= core.MaxRetries; retry++ {
+		commitments, commitmentErr = voteManager.Commitments(&callOpts, stakerId)
+		if commitmentErr != nil {
+			Retry(retry, "Error in fetching commitment: ", commitmentErr)
+			continue
+		}
+		break
+	}
+	if commitmentErr != nil {
 		return [32]byte{}, err
 	}
 	return commitments.CommitmentHash, nil
@@ -51,25 +63,100 @@ func GetInfluenceSnapshot(client *ethclient.Client, address string, epoch uint32
 	if err != nil {
 		return nil, err
 	}
-	return voteManager.GetInfluenceSnapshot(&callOpts, epoch, stakerId)
+	var (
+		influenceSnapshot *big.Int
+		influenceErr      error
+	)
+	for retry := 1; retry <= core.MaxRetries; retry++ {
+		influenceSnapshot, influenceErr = voteManager.GetInfluenceSnapshot(&callOpts, epoch, stakerId)
+		if influenceErr != nil {
+			Retry(retry, "Error in fetching influence snapshot: ", influenceErr)
+			continue
+		}
+		break
+	}
+	if influenceErr != nil {
+		return nil, influenceErr
+	}
+	return influenceSnapshot, nil
 }
 
 func GetTotalInfluenceRevealed(client *ethclient.Client, address string, epoch uint32) (*big.Int, error) {
 	voteManager, callOpts := getVoteManagerWithOpts(client, address)
-	return voteManager.GetTotalInfluenceRevealed(&callOpts, epoch)
+	var (
+		totalInfluenceRevealed *big.Int
+		influenceErr           error
+	)
+	for retry := 1; retry <= core.MaxRetries; retry++ {
+		totalInfluenceRevealed, influenceErr = voteManager.GetTotalInfluenceRevealed(&callOpts, epoch)
+		if influenceErr != nil {
+			Retry(retry, "Error in fetching influence snapshot: ", influenceErr)
+			continue
+		}
+		break
+	}
+	if influenceErr != nil {
+		return nil, influenceErr
+	}
+	return totalInfluenceRevealed, nil
 }
 
 func GetRandaoHash(client *ethclient.Client, address string) ([32]byte, error) {
 	voteManager, callOpts := getVoteManagerWithOpts(client, address)
-	return voteManager.GetRandaoHash(&callOpts)
+	var (
+		randaoHash [32]byte
+		randaoErr  error
+	)
+	for retry := 1; retry <= core.MaxRetries; retry++ {
+		randaoHash, randaoErr = voteManager.GetRandaoHash(&callOpts)
+		if randaoErr != nil {
+			Retry(retry, "Error in fetching randao hash: ", randaoErr)
+			continue
+		}
+		break
+	}
+	if randaoErr != nil {
+		return [32]byte{}, randaoErr
+	}
+	return randaoHash, nil
 }
 
 func GetEpochLastCommitted(client *ethclient.Client, address string, stakerId uint32) (uint32, error) {
 	voteManager, callOpts := getVoteManagerWithOpts(client, address)
-	return voteManager.GetEpochLastCommitted(&callOpts, stakerId)
+	var (
+		epochLastCommitted uint32
+		err                error
+	)
+	for retry := 1; retry <= core.MaxRetries; retry++ {
+		epochLastCommitted, err = voteManager.GetEpochLastCommitted(&callOpts, stakerId)
+		if err != nil {
+			Retry(retry, "Error in fetching epoch last committed: ", err)
+			continue
+		}
+		break
+	}
+	if err != nil {
+		return 0, err
+	}
+	return epochLastCommitted, nil
 }
 
 func GetEpochLastRevealed(client *ethclient.Client, address string, stakerId uint32) (uint32, error) {
 	voteManager, callOpts := getVoteManagerWithOpts(client, address)
-	return voteManager.GetEpochLastRevealed(&callOpts, stakerId)
+	var (
+		epochLastRevealed uint32
+		err                error
+	)
+	for retry := 1; retry <= core.MaxRetries; retry++ {
+		epochLastRevealed, err = voteManager.GetEpochLastRevealed(&callOpts, stakerId)
+		if err != nil {
+			Retry(retry, "Error in fetching epoch last revealed: ", err)
+			continue
+		}
+		break
+	}
+	if err != nil {
+		return 0, err
+	}
+	return epochLastRevealed, nil
 }
