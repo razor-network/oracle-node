a3055f65d6ef74ddcdeba5ea6a0418e38c1e00df
Ashish Mishra <ashish10677@gmail.com>
2021-09-14 20:58:01 +0530

    Implement retry for block manager functions.
    Signed-off-by: Ashish Mishra <ashish10677@gmail.com>

--- utils/block.go
+++ utils/block.go
@@ -2,20 +2,48 @@ package utils
 
 import (
 	"github.com/ethereum/go-ethereum/ethclient"
-	"razor/pkg/bindings"
+	"razor/core"
+	"razor/core/types"
 )
 
 func GetNumberOfProposedBlocks(client *ethclient.Client, address string, epoch uint32) (uint8, error) {
 	blockManager := GetBlockManager(client)
 	callOpts := GetOptions(false, address, "")
-	return blockManager.GetNumProposedBlocks(&callOpts, epoch)
+	var (
+		numProposedBlocks uint8
+		err       error
+	)
+	for retry := 1; retry <= core.MaxRetries; retry++ {
+		numProposedBlocks, err = blockManager.GetNumProposedBlocks(&callOpts, epoch)
+		if err != nil {
+			Retry(retry, "Error in fetching numProposedBlocks: ", err)
+			continue
+		}
+		break
+	}
+	if err != nil {
+		return 0, err
+	}
+	return numProposedBlocks, nil
 }
 
-func GetProposedBlock(client *ethclient.Client, address string, epoch uint32, proposedBlock uint8) (struct {
-	Block        bindings.StructsBlock
-	BlockMedians []uint32
-}, error) {
+func GetProposedBlock(client *ethclient.Client, address string, epoch uint32, proposedBlockId uint8) (types.Block, error) {
 	blockManager := GetBlockManager(client)
 	callOpts := GetOptions(false, address, "")
-	return blockManager.GetProposedBlock(&callOpts, epoch, proposedBlock)
+	var (
+		proposedBlock types.Block
+		err       error
+	)
+	for retry := 1; retry <= core.MaxRetries; retry++ {
+		proposedBlock, err = blockManager.GetProposedBlock(&callOpts, epoch, proposedBlockId)
+		if err != nil {
+			Retry(retry, "Error in fetching proposed block: ", err)
+			continue
+		}
+		break
+	}
+	if err != nil {
+		return types.Block{}, err
+	}
+	return proposedBlock, nil
 }
