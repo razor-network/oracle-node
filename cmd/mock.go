27a647d84e9064e9a53e6382ab0333c6303916ab
Yashk767 <yash.kurlekar2017@gmail.com>
2021-09-15 19:06:51 +0530

    minor changes

--- cmd/mock.go
+++ cmd/mock.go
@@ -16,8 +16,6 @@ type TokenManagerMock struct{}
 
 type TransactionMock struct{}
 
-type StakeManagerMock struct{}
-
 var GetTokenManagerMock func(*ethclient.Client) *bindings.RAZOR
 
 var GetOptionsMock func(bool, string, string) bind.CallOpts
@@ -36,38 +34,34 @@ var HashMock func(*Types.Transaction) common.Hash
 
 var StakeMock func(*bind.TransactOpts, uint32, *big.Int, *ethclient.Client) (*Types.Transaction, error)
 
-func (r UtilsMock) GetTokenManager(client *ethclient.Client) *bindings.RAZOR {
+func (u UtilsMock) GetTokenManager(client *ethclient.Client) *bindings.RAZOR {
 	return GetTokenManagerMock(client)
 }
 
-func (r UtilsMock) GetOptions(pending bool, from string, blockNumber string) bind.CallOpts {
+func (u UtilsMock) GetOptions(pending bool, from string, blockNumber string) bind.CallOpts {
 	return GetOptionsMock(pending, from, blockNumber)
 }
 
-func (r UtilsMock) GetTxnOpts(transactionData types.TransactionOptions) *bind.TransactOpts {
+func (u UtilsMock) GetTxnOpts(transactionData types.TransactionOptions) *bind.TransactOpts {
 	return GetTxnOptsMock(transactionData)
 }
 
-func (r UtilsMock) WaitForBlockCompletion(client *ethclient.Client, hashToRead string) int {
+func (u UtilsMock) WaitForBlockCompletion(client *ethclient.Client, hashToRead string) int {
 	return WaitForBlockCompletionMock(client, hashToRead)
 }
 
-func (r UtilsMock) WaitForCommitState(client *ethclient.Client, accountAddress string, action string) (uint32, error) {
+func (u UtilsMock) WaitForCommitState(client *ethclient.Client, accountAddress string, action string) (uint32, error) {
 	return WaitForCommitStateMock(client, accountAddress, action)
 }
 
-func (r TokenManagerMock) Allowance(opts *bind.CallOpts, owner common.Address, spender common.Address, client *ethclient.Client) (*big.Int, error) {
+func (tokenManagerMock TokenManagerMock) Allowance(opts *bind.CallOpts, owner common.Address, spender common.Address, client *ethclient.Client) (*big.Int, error) {
 	return AllowanceMock(opts, owner, spender, client)
 }
 
-func (r TokenManagerMock) Approve(opts *bind.TransactOpts, spender common.Address, amount *big.Int, client *ethclient.Client) (*Types.Transaction, error) {
+func (tokenManagerMock TokenManagerMock) Approve(opts *bind.TransactOpts, spender common.Address, amount *big.Int, client *ethclient.Client) (*Types.Transaction, error) {
 	return ApproveMock(opts, spender, amount, client)
 }
 
-func (r TransactionMock) Hash(txn *Types.Transaction) common.Hash {
+func (transactionMock TransactionMock) Hash(txn *Types.Transaction) common.Hash {
 	return HashMock(txn)
-}
-
-func (r StakeManagerMock) Stake(txnOpts *bind.TransactOpts, epoch uint32, amount *big.Int, client *ethclient.Client) (*Types.Transaction, error) {
-	return StakeMock(txnOpts, epoch, amount, client)
 }
