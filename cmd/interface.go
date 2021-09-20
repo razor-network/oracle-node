e193043c862e3fd7f3e29a43f88e01c3dcbb9bb7
Yashk767 <yash.kurlekar2017@gmail.com>
2021-09-20 23:03:00 +0530

    Seperate transfer function

--- cmd/interface.go
+++ cmd/interface.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	Types "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/spf13/pflag"
 	"math/big"
 	"razor/core/types"
 	"razor/pkg/bindings"
@@ -15,11 +16,18 @@ type utilsInterface interface {
 	GetOptions(bool, string, string) bind.CallOpts
 	GetTxnOpts(types.TransactionOptions) *bind.TransactOpts
 	WaitForBlockCompletion(*ethclient.Client, string) int
+	AssignPassword(*pflag.FlagSet) string
+ ConnectToClient( string) *ethclient.Client
+ FetchBalance( *ethclient.Client,  string) (*big.Int, error)
+ AssignAmountInWei( *pflag.FlagSet) *big.Int
+ CheckAmountAndBalance( *big.Int,  *big.Int) *big.Int
+ GetAmountInDecimal( *big.Int) *big.Float
 }
 
 type tokenManagerInterface interface {
 	Allowance(*bind.CallOpts, common.Address, common.Address, *ethclient.Client) (*big.Int, error)
 	Approve(*bind.TransactOpts, common.Address, *big.Int, *ethclient.Client) (*Types.Transaction, error)
+	Transfer( *ethclient.Client, *bind.TransactOpts,  common.Address,  *big.Int) (*Types.Transaction, error)
 }
 
 type transactionInterface interface {
