8d3d36132e3bb03e8b5e2fc8bd910109d4df8fda
Yashk767 <yash.kurlekar2017@gmail.com>
2021-09-29 15:52:40 +0530

    Seperate addJobToCollection function

--- cmd/addJobToCollection.go
+++ cmd/addJobToCollection.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/spf13/pflag"
 	"razor/core"
 	"razor/core/types"
 	"razor/utils"
@@ -15,7 +17,7 @@ var addJobToCollectionCmd = &cobra.Command{
 
 Example: 
   ./razor addJobToCollection --address 0x5a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c --collectionId 6 --jobId 7 
-
+eErrer
 Note: 
   This command only works for the admin.
 `,
@@ -24,35 +26,53 @@ Note:
 		if err != nil {
 			log.Fatal("Error in getting config: ", err)
 		}
-
-		password := utils.PasswordPrompt()
-
-		address, _ := cmd.Flags().GetString("address")
-		jobId, _ := cmd.Flags().GetUint8("jobId")
-		collectionId, _ := cmd.Flags().GetUint8("collectionId")
-
-		client := utils.ConnectToClient(config.Provider)
-
-		txnOpts := utils.GetTxnOpts(types.TransactionOptions{
-			Client:         client,
-			Password:       password,
-			AccountAddress: address,
-			ChainId:        core.ChainId,
-			Config:         config,
-		})
-
-		assetManager := utils.GetAssetManager(client)
-		log.Infof("Adding Job %d to collection %d", jobId, collectionId)
-		txn, err := assetManager.AddJobToCollection(txnOpts, collectionId, jobId)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Info("Txn Hash: ", txn.Hash())
-		utils.WaitForBlockCompletion(client, txn.Hash().String())
+		txn, err := addJobToCollection(cmd.Flags(), config, razorUtils, assetManagerUtils, transactionUtils, flagSetUtils)
+		utils.CheckError("AddJobToCollection error: ", err)
+		utils.WaitForBlockCompletion(utils.ConnectToClient(config.Provider), txn.String())
 	},
 }
 
+func addJobToCollection(flagSet *pflag.FlagSet, config types.Configurations, razorUtils utilsInterface, assetManagerUtils assetManagerInterface, transactionUtils transactionInterface, flagSetUtils flagSetInterface) (common.Hash, error) {
+
+	password := razorUtils.AssignPassword(flagSet)
+	address, err := flagSetUtils.GetStringAddress(flagSet)
+	if err != nil {
+		return core.NilHash, err
+	}
+	jobId, err := flagSetUtils.GetUint8JobId(flagSet)
+	if err != nil {
+		return core.NilHash, err
+	}
+	collectionId, err := flagSetUtils.GetUint8CollectionId(flagSet)
+	if err != nil {
+		return core.NilHash, err
+	}
+	client := razorUtils.ConnectToClient(config.Provider)
+
+	txnOpts := razorUtils.GetTxnOpts(types.TransactionOptions{
+		Client:         client,
+		Password:       password,
+		AccountAddress: address,
+		ChainId:        core.ChainId,
+		Config:         config,
+	})
+
+	log.Infof("Adding Job %d to collection %d", jobId, collectionId)
+	txn, err := assetManagerUtils.AddJobToCollection(client, txnOpts, collectionId, jobId)
+	if err != nil {
+		return core.NilHash, err
+	}
+	log.Info("Txn Hash: ", transactionUtils.Hash(txn))
+	return transactionUtils.Hash(txn), nil
+}
+
 func init() {
+
+	razorUtils = Utils{}
+	assetManagerUtils = AssetManagerUtils{}
+	transactionUtils = TransactionUtils{}
+	flagSetUtils = FlagSetUtils{}
+
 	rootCmd.AddCommand(addJobToCollectionCmd)
 
 	var (
