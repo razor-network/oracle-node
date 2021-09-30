64f10e096777a588f1a30931f4905a68532b5369
Oxhimanshu <crytosatoshi@gmail.com>
2021-09-30 12:44:47 +0530

    updated package.json & contracts v0.1.62

--- core/constants.go
+++ core/constants.go
@@ -7,12 +7,12 @@ import (
 )
 
 //TODO: Change these addresses to the deployed address.
-var StakeManagerAddress = "0x92d5fB140fB93E0790baD45f945677ABBC0239c3"
-var RAZORAddress = "0xA02C76D746877d3bB5df1fD3c4130D642c16ab49"
-var ParametersAddress = "0x31E62E022f241bfE8c3Bc83DE8c090af7a67DdD6"
-var AssetManagerAddress = "0x80280d1D25558Bd23712720266428Ba3AfaD2e0B"
-var VoteManagerAddress = "0xD0dcD208Cf140cC160E8e5423a9d76458c58AC6E"
-var BlockManagerAddress = "0x8eAC317759167b0901cf0b2548a91321F62B3f2B"
+var StakeManagerAddress = "0x6133d9f3Dd76C2D405652bF284728b8AAB665d1b"
+var RAZORAddress = "0xc89FadB7A04813f4d68D39363ECa86cb232897c3"
+var ParametersAddress = "0xC463712DF07c4401665Fd7765915007C08d4182a"
+var AssetManagerAddress = "0x4b0e123589bAaD54D776aE68d6A9357804e92e70"
+var VoteManagerAddress = "0xbD7945A5B9Fb1697995f715bE82930977707eeA8"
+var BlockManagerAddress = "0xbDa74791465aEA005eF001b74521867d61606D5d"
 
 var StateLength uint64 = 60
 var EpochLength int64 = 300
