1a522d5367115de2dbe43826b6073dab9dd2aa3d
Ashish Kumar Mishra <ashish10677@gmail.com>
2021-10-06 15:30:05 +0530

    Add updateCollection Command.
    Signed-off-by: Ashish Kumar Mishra <ashish10677@gmail.com>

---
+++ cmd/updateCollection.go
@@ -0,0 +1,101 @@
+package cmd
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"razor/core"
+	"razor/core/types"
+	"razor/utils"
+)
+
+var updateCollectionCmd = &cobra.Command{
+	Use:   "updateCollection",
+	Short: "updateCollection can be used to update an existing collection",
+	Long: `A collection is a group of jobs that reports the aggregated value of jobs. updateCollection can be used to modify an already existing collection.
+
+Example: 
+  ./razor updateCollection --address 0x5a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c --aggregation 2 --power 3 --collectionId 3
+
+Note: 
+  This command only works for the admin.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		config, err := GetConfigData()
+		utils.CheckError("Error in getting config: ", err)
+
+		txn, err := updateCollection(cmd.Flags(), config, razorUtils, assetManagerUtils, transactionUtils, flagSetUtils)
+		utils.CheckError("CreateCollection error: ", err)
+		utils.WaitForBlockCompletion(utils.ConnectToClient(config.Provider), txn.String())
+	},
+}
+
+func updateCollection(flagSet *pflag.FlagSet, config types.Configurations, razorUtils utilsInterface, assetManagerUtils assetManagerInterface, transactionUtils transactionInterface, flagSetUtils flagSetInterface) (common.Hash, error) {
+	password := razorUtils.AssignPassword(flagSet)
+	address, err := flagSetUtils.GetStringAddress(flagSet)
+	if err != nil {
+		return core.NilHash, err
+	}
+	collectionId, err := flagSetUtils.GetUint8CollectionId(flagSet)
+	if err != nil {
+		return core.NilHash, err
+	}
+	aggregation, err := flagSetUtils.GetUint32Aggregation(flagSet)
+	if err != nil {
+		return core.NilHash, err
+	}
+	power, err := flagSetUtils.GetInt8Power(flagSet)
+	if err != nil {
+		return core.NilHash, err
+	}
+
+	client := razorUtils.ConnectToClient(config.Provider)
+
+	txnOpts := razorUtils.GetTxnOpts(types.TransactionOptions{
+		Client:         client,
+		Password:       password,
+		AccountAddress: address,
+		ChainId:        core.ChainId,
+		Config:         config,
+	})
+
+	txn, err := assetManagerUtils.UpdateCollection(client, txnOpts, collectionId, aggregation, power)
+	if err != nil {
+		log.Error("Error in creating collection")
+		return core.NilHash, err
+	}
+	log.Info("Creating collection...")
+	log.Info("Txn Hash: ", transactionUtils.Hash(txn))
+	return transactionUtils.Hash(txn), nil
+}
+
+func init() {
+	razorUtils = Utils{}
+	assetManagerUtils = AssetManagerUtils{}
+	transactionUtils = TransactionUtils{}
+	flagSetUtils = FlagSetUtils{}
+
+	rootCmd.AddCommand(updateCollectionCmd)
+
+	var (
+		Account           string
+		CollectionId      uint8
+		AggregationMethod uint32
+		Password          string
+		Power             int8
+	)
+
+	updateCollectionCmd.Flags().StringVarP(&Account, "address", "a", "", "address of the job creator")
+	updateCollectionCmd.Flags().Uint8VarP(&CollectionId, "collectionId", "", 0, "collection id to be modified")
+	updateCollectionCmd.Flags().Uint32VarP(&AggregationMethod, "aggregation", "", 1, "aggregation method to be used")
+	updateCollectionCmd.Flags().Int8VarP(&Power, "power", "", 0, "multiplier for the collection")
+	updateCollectionCmd.Flags().StringVarP(&Password, "password", "", "", "password path of job creator to protect the keystore")
+
+	collectionIdErr := updateCollectionCmd.MarkFlagRequired("collectionId")
+	utils.CheckError("Collection Id error: ", collectionIdErr)
+	addrErr := updateCollectionCmd.MarkFlagRequired("address")
+	utils.CheckError("Address Error: ", addrErr)
+	powerErr := updateCollectionCmd.MarkFlagRequired("power")
+	utils.CheckError("Power Error: ", powerErr)
+
+}
