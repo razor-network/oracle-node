226abc22e745a8b71a9af1ca40a20a34aa312a4e
Ashish Kumar Mishra <ashish10677@gmail.com>
2021-11-18 13:26:45 +0530

    Initialized sentry to the project.
    Signed-off-by: Ashish Kumar Mishra <ashish10677@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "razor/cmd"
+import (
+	"github.com/getsentry/sentry-go"
+	"log"
+	"razor/cmd"
+	"time"
+)
 
 func main() {
+
+	sentrySyncTransport := sentry.NewHTTPSyncTransport()
+	sentrySyncTransport.Timeout = time.Second * 3
+
+	err := sentry.Init(sentry.ClientOptions{
+		Environment: "dev",
+		Release:     "razor-go@0.1.76",
+		Debug:       true,
+		Transport:   sentrySyncTransport,
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	defer sentry.Flush(2 * time.Second)
+	defer sentry.Recover()
 	cmd.Execute()
 }
