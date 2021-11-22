9c4855645df48063491836b51f244b0474ff0eab
Yashk767 <yash.kurlekar2017@gmail.com>
2021-11-22 14:34:55 +0530

    Modified functions with utilsStruct

--- cmd/cmd-utils.go
+++ cmd/cmd-utils.go
@@ -1,35 +1,37 @@
 package cmd
 
 import (
-	"razor/utils"
 	"time"
 
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func GetEpochAndState(client *ethclient.Client, accountAddress string) (uint32, int64, error) {
-	epoch, err := utils.GetEpoch(client)
+func GetEpochAndState(client *ethclient.Client, accountAddress string, utilsStruct UtilsStruct) (uint32, int64, error) {
+	epoch, err := utilsStruct.razorUtils.GetEpoch(client)
 	if err != nil {
 		return 0, 0, err
 	}
-	bufferPercent, err := getBufferPercent()
+	bufferPercent, err := utilsStruct.razorUtils.getBufferPercent()
 	if err != nil {
 		return 0, 0, err
 	}
-	state, err := utils.GetDelayedState(client, bufferPercent)
+	state, err := utilsStruct.razorUtils.GetDelayedState(client, bufferPercent)
 	if err != nil {
 		return 0, 0, err
 	}
 	log.Debug("Epoch ", epoch)
-	log.Debug("State ", utils.GetStateName(state))
+	log.Debug("State ", utilsStruct.razorUtils.GetStateName(state))
 	return epoch, state, nil
 }
 
-func WaitForAppropriateState(client *ethclient.Client, accountAddress string, action string, states ...int) (uint32, error) {
+func WaitForAppropriateState(client *ethclient.Client, accountAddress string, action string, utilsStruct UtilsStruct, states ...int) (uint32, error) {
 	for {
-		epoch, state, err := GetEpochAndState(client, accountAddress)
-		utils.CheckError("Error in fetching epoch and state: ", err)
-		if !utils.Contains(states, int(state)) {
+		epoch, state, err := utilsStruct.cmdUtils.GetEpochAndState(client, accountAddress, utilsStruct)
+		if err != nil {
+			log.Error("Error in fetching epoch and state: ", err)
+			return epoch, err
+		}
+		if !utilsStruct.razorUtils.Contains(states, int(state)) {
 			log.Debugf("Can only %s during %d state(s). Retrying in 5 seconds...", action, states)
 			time.Sleep(5 * time.Second)
 		} else {
@@ -38,10 +40,13 @@ func WaitForAppropriateState(client *ethclient.Client, accountAddress string, ac
 	}
 }
 
-func WaitIfCommitState(client *ethclient.Client, accountAddress string, action string) (uint32, error) {
+func WaitIfCommitState(client *ethclient.Client, accountAddress string, action string, utilsStruct UtilsStruct) (uint32, error) {
 	for {
-		epoch, state, err := GetEpochAndState(client, accountAddress)
-		utils.CheckError("Error in fetching epoch and state: ", err)
+		epoch, state, err := utilsStruct.cmdUtils.GetEpochAndState(client, accountAddress, utilsStruct)
+		if err != nil {
+			log.Error("Error in fetching epoch and state: ", err)
+			return epoch, err
+		}
 		if state == 0 || state == -1 {
 			log.Debugf("Cannot perform %s during commit state. Retrying in 5 seconds...", action)
 			time.Sleep(5 * time.Second)
