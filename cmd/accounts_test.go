549b9a13159f31f5d443033f41a38a54b3b410ec
Yashk767 <yash.kurlekar2017@gmail.com>
2021-11-29 18:23:26 +0530

    Added tests for GetPrivateKey(), Sign()

--- cmd/accounts_test.go
+++ cmd/accounts_test.go
@@ -73,34 +73,73 @@ func TestCreateAccount(t *testing.T) {
 	}
 }
 
-func TestGetPrivateKey(t *testing.T) {
-
-}
-
 func TestSign(t *testing.T) {
+	var hash []byte
+	var account types.Account
+	var defaultPath string
+	var privateKey *ecdsa.PrivateKey
+	var signature []byte
+
+	utilsStruct := UtilsStruct{
+		cmdUtils:    UtilsCmdMock{},
+		cryptoUtils: CryptoMock{},
+	}
+
 	type args struct {
-		hash        []byte
-		account     types.Account
-		defaultPath string
-		utilsStruct UtilsStruct
+		privateKey   *ecdsa.PrivateKey
+		signature    []byte
+		signatureErr error
 	}
 	tests := []struct {
 		name    string
 		args    args
 		want    []byte
-		wantErr bool
+		wantErr error
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test 1: When Sign function returns no error",
+			args: args{
+				privateKey:   privateKey,
+				signature:    signature,
+				signatureErr: nil,
+			},
+			want:    signature,
+			wantErr: nil,
+		},
+		{
+			name: "Test 2: When Sign function returns error",
+			args: args{
+				privateKey:   privateKey,
+				signatureErr: nil,
+			},
+			want:    nil,
+			wantErr: nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Sign(tt.args.hash, tt.args.account, tt.args.defaultPath, tt.args.utilsStruct)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Sign() error = %v, wantErr %v", err, tt.wantErr)
-				return
+			GetPrivateKeyMock = func(string, string, string, UtilsStruct) *ecdsa.PrivateKey {
+				return tt.args.privateKey
 			}
+
+			SignMock = func([]byte, *ecdsa.PrivateKey) ([]byte, error) {
+				return tt.args.signature, tt.args.signatureErr
+			}
+
+			got, err := Sign(hash, account, defaultPath, utilsStruct)
+
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Sign() got = %v, want %v", got, tt.want)
+			}
+
+			if err == nil || tt.wantErr == nil {
+				if err != tt.wantErr {
+					t.Errorf("Error for Sign function, got = %v, want = %v", err, tt.wantErr)
+				}
+			} else {
+				if err.Error() != tt.wantErr.Error() {
+					t.Errorf("Error for Sign function, got = %v, want = %v", err, tt.wantErr)
+				}
 			}
 		})
 	}
@@ -186,6 +225,72 @@ func Test_getPrivateKeyFromKeystore(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("Private key from GetPrivateKey, got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetPrivateKey(t *testing.T) {
+	var password string
+	var keystorePath string
+	var privateKey *ecdsa.PrivateKey
+
+	utilsStruct := UtilsStruct{
+		cmdUtils:      UtilsCmdMock{},
+		keystoreUtils: KeystoreMock{},
+	}
+
+	accountsList := []accounts.Account{
+		{Address: common.HexToAddress("0x000000000000000000000000000000000000dea1"),
+			URL: accounts.URL{Scheme: "TestKeyScheme", Path: "test/key/path"},
+		},
+		{Address: common.HexToAddress("0x000000000000000000000000000000000000dea2"),
+			URL: accounts.URL{Scheme: "TestKeyScheme", Path: "test/key/path"},
+		},
+	}
+
+	type args struct {
+		address    string
+		accounts   []accounts.Account
+		privateKey *ecdsa.PrivateKey
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ecdsa.PrivateKey
+	}{
+		{
+			name: "Test 1: When input address is present in accountsList",
+			args: args{
+				address:    "0x000000000000000000000000000000000000dea1",
+				accounts:   accountsList,
+				privateKey: privateKey,
+			},
+			want: privateKey,
+		},
+		{
+			name: "Test 2: When input address is npt present in accountsList",
+			args: args{
+				address:    "0x000000000000000000000000000000000000dea3",
+				accounts:   accountsList,
+				privateKey: privateKey,
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			AccountsMock = func(string) []accounts.Account {
+				return tt.args.accounts
+			}
+
+			getPrivateKeyFromKeystoreMock = func(string, string, UtilsStruct) *ecdsa.PrivateKey {
+				return tt.args.privateKey
+			}
+
+			got := GetPrivateKey(tt.args.address, password, keystorePath, utilsStruct)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetPrivateKey() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
