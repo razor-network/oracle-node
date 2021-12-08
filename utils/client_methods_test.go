6dc427b110a9ae1ede9e7024dcc76315b2140a69
Yashk767 <yash.kurlekar2017@gmail.com>
2021-12-08 22:52:53 +0530

    Added mocks and tests

---
+++ utils/client_methods_test.go
@@ -0,0 +1,338 @@
+package utils
+
+import (
+	"context"
+	"errors"
+	"github.com/avast/retry-go"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"math/big"
+	"reflect"
+	"testing"
+)
+
+func TestBalanceAtWithRetry(t *testing.T) {
+	var client *ethclient.Client
+	var account common.Address
+
+	razorUtils := RazorUtilsMock{}
+	type args struct {
+		balance    *big.Int
+		balanceErr error
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    *big.Int
+		wantErr bool
+	}{
+		{
+			name: "Test 1: When BalanceAtWithRetry() executes successfully",
+			args: args{
+				balance: big.NewInt(1000),
+			},
+			want:    big.NewInt(1000),
+			wantErr: false,
+		},
+		{
+			name: "Test 2: When there is an error in getting balance",
+			args: args{
+				balanceErr: errors.New("balance error"),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			BalanceAtMock = func(*ethclient.Client, context.Context, common.Address, *big.Int) (*big.Int, error) {
+				return tt.args.balance, tt.args.balanceErr
+			}
+
+			RetryAttemptsMock = func(uint) retry.Option {
+				return retry.Attempts(1)
+			}
+
+			got, err := BalanceAtWithRetry(client, account, razorUtils)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BalanceAtWithRetry() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BalanceAtWithRetry() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEstimateGasWithRetry(t *testing.T) {
+	var client *ethclient.Client
+	var message ethereum.CallMsg
+	razorUtils := RazorUtilsMock{}
+
+	type args struct {
+		gasLimit    uint64
+		gasLimitErr error
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    uint64
+		wantErr bool
+	}{
+		{
+			name: "Test 1: When EstimateGasWithRetry executes successfully",
+			args: args{
+				gasLimit: 2,
+			},
+			want:    2,
+			wantErr: false,
+		},
+		{
+			name: "Test 2: When there is an error in getting gasLimit",
+			args: args{
+				gasLimitErr: errors.New("gasLimit error"),
+			},
+			want:    0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			EstimateGasMock = func(*ethclient.Client, context.Context, ethereum.CallMsg) (uint64, error) {
+				return tt.args.gasLimit, tt.args.gasLimitErr
+			}
+
+			RetryAttemptsMock = func(uint) retry.Option {
+				return retry.Attempts(1)
+			}
+
+			got, err := EstimateGasWithRetry(client, message, razorUtils)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("EstimateGasWithRetry() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("EstimateGasWithRetry() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFilterLogsWithRetry(t *testing.T) {
+	var client *ethclient.Client
+	var query ethereum.FilterQuery
+
+	razorUtils := RazorUtilsMock{}
+	type args struct {
+		logs    []types.Log
+		logsErr error
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []types.Log
+		wantErr bool
+	}{
+		{
+			name: "Test 1: When FilterLogsWithRetry executes successfully",
+			args: args{
+				logs: []types.Log{},
+			},
+			want:    []types.Log{},
+			wantErr: false,
+		},
+		{
+			name: "Test 2: When there is an error in getting filterLogs",
+			args: args{
+				logsErr: errors.New("logs error"),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			FilterLogsMock = func(*ethclient.Client, context.Context, ethereum.FilterQuery) ([]types.Log, error) {
+				return tt.args.logs, tt.args.logsErr
+			}
+
+			RetryAttemptsMock = func(uint) retry.Option {
+				return retry.Attempts(1)
+			}
+
+			got, err := FilterLogsWithRetry(client, query, razorUtils)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FilterLogsWithRetry() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FilterLogsWithRetry() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetLatestBlockWithRetry(t *testing.T) {
+	var client *ethclient.Client
+
+	razorUtils := RazorUtilsMock{}
+
+	type args struct {
+		latestHeader    *types.Header
+		latestHeaderErr error
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *types.Header
+		wantErr bool
+	}{
+		{
+			name: "Test 1: When GetLatestBlockWithRetry executes successfully",
+			args: args{
+				latestHeader: &types.Header{},
+			},
+			want:    &types.Header{},
+			wantErr: false,
+		},
+		{
+			name: "Test 2: When there is an error in getting latestHeader",
+			args: args{
+				latestHeaderErr: errors.New("header error"),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			HeaderByNumberMock = func(*ethclient.Client, context.Context, *big.Int) (*types.Header, error) {
+				return tt.args.latestHeader, tt.args.latestHeaderErr
+			}
+
+			RetryAttemptsMock = func(uint) retry.Option {
+				return retry.Attempts(1)
+			}
+			got, err := GetLatestBlockWithRetry(client, razorUtils)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetLatestBlockWithRetry() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetLatestBlockWithRetry() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetPendingNonceAtWithRetry(t *testing.T) {
+	var client *ethclient.Client
+	var accountAddress common.Address
+
+	razorUtils := RazorUtilsMock{}
+	type args struct {
+		nonce    uint64
+		nonceErr error
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    uint64
+		wantErr bool
+	}{
+		{
+			name: "Test 1: When BalanceAtWithRetry() executes successfully",
+			args: args{
+				nonce: 2,
+			},
+			want:    2,
+			wantErr: false,
+		},
+		{
+			name: "Test 2: When there is an error in getting nonce",
+			args: args{
+				nonceErr: errors.New("nonce error"),
+			},
+			want:    0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			PendingNonceAtMock = func(client *ethclient.Client, ctx context.Context, account common.Address) (uint64, error) {
+				return tt.args.nonce, tt.args.nonceErr
+			}
+
+			RetryAttemptsMock = func(uint) retry.Option {
+				return retry.Attempts(1)
+			}
+
+			got, err := GetPendingNonceAtWithRetry(client, accountAddress, razorUtils)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetPendingNonceAtWithRetry() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetPendingNonceAtWithRetry() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSuggestGasPriceWithRetry(t *testing.T) {
+	var client *ethclient.Client
+	razorUtils := RazorUtilsMock{}
+
+	type args struct {
+		gasPrice    *big.Int
+		gasPriceErr error
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *big.Int
+		wantErr bool
+	}{
+		{
+			name: "Test 1: When SuggestGasPriceWithRetry() executes successfully",
+			args: args{
+				gasPrice: big.NewInt(1),
+			},
+			want:    big.NewInt(1),
+			wantErr: false,
+		},
+		{
+			name: "Test 2: When there is an error in getting gasPrice",
+			args: args{
+				gasPriceErr: errors.New("gasPrice Error"),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			SuggestGasPriceMock = func(*ethclient.Client, context.Context) (*big.Int, error) {
+				return tt.args.gasPrice, tt.args.gasPriceErr
+			}
+
+			RetryAttemptsMock = func(uint) retry.Option {
+				return retry.Attempts(1)
+			}
+
+			got, err := SuggestGasPriceWithRetry(client, razorUtils)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SuggestGasPriceWithRetry() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SuggestGasPriceWithRetry() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- utils/mock.go
+++ utils/mock.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"context"
 	"crypto/ecdsa"
+	"github.com/avast/retry-go"
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -16,7 +18,7 @@ import (
 
 type RazorUtilsMock struct{}
 
-var SuggestGasPriceWithRetryMock func(*ethclient.Client) (*big.Int, error)
+var SuggestGasPriceWithRetryMock func(*ethclient.Client, RazorUtilsInterface) (*big.Int, error)
 
 var MultiplyFloatAndBigIntMock func(*big.Int, float64) *big.Int
 
@@ -24,17 +26,17 @@ var parseMock func(io.Reader) (abi.ABI, error)
 
 var PackMock func(abi.ABI, string, ...interface{}) ([]byte, error)
 
-var EstimateGasWithRetryMock func(*ethclient.Client, ethereum.CallMsg) (uint64, error)
+var EstimateGasWithRetryMock func(*ethclient.Client, ethereum.CallMsg, RazorUtilsInterface) (uint64, error)
 
 var increaseGasLimitValueMock func(*ethclient.Client, uint64, float32, RazorUtilsInterface) (uint64, error)
 
-var GetLatestBlockWithRetryMock func(*ethclient.Client) (*types.Header, error)
+var GetLatestBlockWithRetryMock func(*ethclient.Client, RazorUtilsInterface) (*types.Header, error)
 
 var GetDefaultPathMock func() (string, error)
 
 var GetPrivateKeyMock func(string, string, string, accounts.AccountInterface) *ecdsa.PrivateKey
 
-var GetPendingNonceAtWithRetryMock func(*ethclient.Client, common.Address) (uint64, error)
+var GetPendingNonceAtWithRetryMock func(*ethclient.Client, common.Address, RazorUtilsInterface) (uint64, error)
 
 var getGasPriceMock func(*ethclient.Client, Types.Configurations, RazorUtilsInterface) *big.Int
 
@@ -42,8 +44,22 @@ var getGasLimitMock func(Types.TransactionOptions, *bind.TransactOpts, RazorUtil
 
 var NewKeyedTransactorWithChainIDMock func(*ecdsa.PrivateKey, *big.Int) (*bind.TransactOpts, error)
 
-func (utilsMock RazorUtilsMock) SuggestGasPriceWithRetry(client *ethclient.Client) (*big.Int, error) {
-	return SuggestGasPriceWithRetryMock(client)
+var PendingNonceAtMock func(*ethclient.Client, context.Context, common.Address) (uint64, error)
+
+var HeaderByNumberMock func(*ethclient.Client, context.Context, *big.Int) (*types.Header, error)
+
+var SuggestGasPriceMock func(*ethclient.Client, context.Context) (*big.Int, error)
+
+var EstimateGasMock func(*ethclient.Client, context.Context, ethereum.CallMsg) (uint64, error)
+
+var FilterLogsMock func(*ethclient.Client, context.Context, ethereum.FilterQuery) ([]types.Log, error)
+
+var BalanceAtMock func(*ethclient.Client, context.Context, common.Address, *big.Int) (*big.Int, error)
+
+var RetryAttemptsMock func(uint) retry.Option
+
+func (utilsMock RazorUtilsMock) SuggestGasPriceWithRetry(client *ethclient.Client, razorUtils RazorUtilsInterface) (*big.Int, error) {
+	return SuggestGasPriceWithRetryMock(client, razorUtils)
 }
 
 func (utilsMock RazorUtilsMock) MultiplyFloatAndBigInt(bigIntVal *big.Int, floatingVal float64) *big.Int {
@@ -58,16 +74,16 @@ func (utilsMock RazorUtilsMock) Pack(parsedData abi.ABI, name string, args ...in
 	return PackMock(parsedData, name, args)
 }
 
-func (utilsMock RazorUtilsMock) EstimateGasWithRetry(client *ethclient.Client, message ethereum.CallMsg) (uint64, error) {
-	return EstimateGasWithRetryMock(client, message)
+func (utilsMock RazorUtilsMock) EstimateGasWithRetry(client *ethclient.Client, message ethereum.CallMsg, razorUtils RazorUtilsInterface) (uint64, error) {
+	return EstimateGasWithRetryMock(client, message, razorUtils)
 }
 
 func (utilsMock RazorUtilsMock) increaseGasLimitValue(client *ethclient.Client, gasLimit uint64, gasLimitMultiplier float32, razorUtils RazorUtilsInterface) (uint64, error) {
 	return increaseGasLimitValueMock(client, gasLimit, gasLimitMultiplier, razorUtils)
 }
 
-func (utilsMock RazorUtilsMock) GetLatestBlockWithRetry(client *ethclient.Client) (*types.Header, error) {
-	return GetLatestBlockWithRetryMock(client)
+func (utilsMock RazorUtilsMock) GetLatestBlockWithRetry(client *ethclient.Client, razorUtils RazorUtilsInterface) (*types.Header, error) {
+	return GetLatestBlockWithRetryMock(client, razorUtils)
 }
 
 func (utilsMock RazorUtilsMock) GetDefaultPath() (string, error) {
@@ -78,8 +94,8 @@ func (utilsMock RazorUtilsMock) GetPrivateKey(address string, password string, k
 	return GetPrivateKeyMock(address, password, keystorePath, accountUtils)
 }
 
-func (utilsMock RazorUtilsMock) GetPendingNonceAtWithRetry(client *ethclient.Client, accountAddress common.Address) (uint64, error) {
-	return GetPendingNonceAtWithRetryMock(client, accountAddress)
+func (utilsMock RazorUtilsMock) GetPendingNonceAtWithRetry(client *ethclient.Client, accountAddress common.Address, razorUtils RazorUtilsInterface) (uint64, error) {
+	return GetPendingNonceAtWithRetryMock(client, accountAddress, razorUtils)
 }
 
 func (utilsMock RazorUtilsMock) getGasPrice(client *ethclient.Client, config Types.Configurations, razorUtils RazorUtilsInterface) *big.Int {
@@ -92,4 +108,32 @@ func (utilsMock RazorUtilsMock) getGasLimit(transactionData Types.TransactionOpt
 
 func (utilsMock RazorUtilsMock) NewKeyedTransactorWithChainID(privateKey *ecdsa.PrivateKey, chainId *big.Int) (*bind.TransactOpts, error) {
 	return NewKeyedTransactorWithChainIDMock(privateKey, chainId)
+}
+
+func (utilsMock RazorUtilsMock) PendingNonceAt(client *ethclient.Client, ctx context.Context, account common.Address) (uint64, error) {
+	return PendingNonceAtMock(client, ctx, account)
+}
+
+func (utilsMock RazorUtilsMock) HeaderByNumber(client *ethclient.Client, ctx context.Context, number *big.Int) (*types.Header, error) {
+	return HeaderByNumberMock(client, ctx, number)
+}
+
+func (utilsMock RazorUtilsMock) SuggestGasPrice(client *ethclient.Client, ctx context.Context) (*big.Int, error) {
+	return SuggestGasPriceMock(client, ctx)
+}
+
+func (utilsMock RazorUtilsMock) EstimateGas(client *ethclient.Client, ctx context.Context, msg ethereum.CallMsg) (uint64, error) {
+	return EstimateGasMock(client, ctx, msg)
+}
+
+func (utilsMock RazorUtilsMock) FilterLogs(client *ethclient.Client, ctx context.Context, q ethereum.FilterQuery) ([]types.Log, error) {
+	return FilterLogsMock(client, ctx, q)
+}
+
+func (utilsMock RazorUtilsMock) BalanceAt(client *ethclient.Client, ctx context.Context, account common.Address, blockNumber *big.Int) (*big.Int, error) {
+	return BalanceAtMock(client, ctx, account, blockNumber)
+}
+
+func (utilsMock RazorUtilsMock) RetryAttempts(numberOfAttempts uint) retry.Option {
+	return RetryAttemptsMock(numberOfAttempts)
 }
