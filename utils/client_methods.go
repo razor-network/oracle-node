34c55bc81b3231965a22a4b8e91db7ca624210de
Yashk767 <yash.kurlekar2017@gmail.com>
2021-12-08 20:00:18 +0530

    Modified functions with razorUtils

--- utils/client_methods.go
+++ utils/client_methods.go
@@ -11,75 +11,74 @@ import (
 	"razor/core"
 )
 
-func GetPendingNonceAtWithRetry(client *ethclient.Client, accountAddress common.Address) (uint64, error) {
+func GetPendingNonceAtWithRetry(client *ethclient.Client, accountAddress common.Address, razorUtils RazorUtilsInterface) (uint64, error) {
 	var (
 		nonce uint64
 		err   error
 	)
 	err = retry.Do(
 		func() error {
-			nonce, err = client.PendingNonceAt(context.Background(), accountAddress)
+			nonce, err = razorUtils.PendingNonceAt(client, context.Background(), accountAddress)
 			if err != nil {
 				log.Error("Error in fetching nonce.... Retrying")
 				return err
 			}
 			return nil
-		},
-	)
+		}, razorUtils.RetryAttempts(core.MaxRetries))
 	if err != nil {
 		return 0, err
 	}
 	return nonce, nil
 }
 
-func GetLatestBlockWithRetry(client *ethclient.Client) (*types.Header, error) {
+func GetLatestBlockWithRetry(client *ethclient.Client, razorUtils RazorUtilsInterface) (*types.Header, error) {
 	var (
 		latestHeader *types.Header
 		err          error
 	)
 	err = retry.Do(
 		func() error {
-			latestHeader, err = client.HeaderByNumber(context.Background(), nil)
+			latestHeader, err = razorUtils.HeaderByNumber(client, context.Background(), nil)
 			if err != nil {
 				log.Error("Error in fetching latest block.... Retrying")
 				return err
 			}
 			return nil
-		}, retry.Attempts(core.MaxRetries))
+		}, razorUtils.RetryAttempts(core.MaxRetries))
 	if err != nil {
 		return nil, err
 	}
 	return latestHeader, nil
 }
 
-func SuggestGasPriceWithRetry(client *ethclient.Client) (*big.Int, error) {
+func SuggestGasPriceWithRetry(client *ethclient.Client, razorUtils RazorUtilsInterface) (*big.Int, error) {
 	var (
 		gasPrice *big.Int
 		err      error
 	)
 	err = retry.Do(
 		func() error {
-			gasPrice, err = client.SuggestGasPrice(context.Background())
+			gasPrice, err = razorUtils.SuggestGasPrice(client, context.Background())
 			if err != nil {
 				log.Error("Error in fetching gas price.... Retrying")
 				return err
 			}
 			return nil
-		}, retry.Attempts(3))
+		}, razorUtils.RetryAttempts(3))
 	if err != nil {
 		return nil, err
 	}
 	return gasPrice, nil
 }
 
-func EstimateGasWithRetry(client *ethclient.Client, message ethereum.CallMsg) (uint64, error) {
+func EstimateGasWithRetry(client *ethclient.Client, message ethereum.CallMsg, razorUtils RazorUtilsInterface) (uint64, error) {
 	var (
 		gasLimit uint64
 		err      error
 	)
 	err = retry.Do(
 		func() error {
-			gasLimit, err = client.EstimateGas(context.Background(), message)
+			gasLimit, err = razorUtils.EstimateGas(client, context.Background(), message)
 			if err != nil {
 				log.Error("Error in estimating gas limit.... Retrying")
 				return err
@@ -92,42 +91,40 @@ func EstimateGasWithRetry(client *ethclient.Client, message ethereum.CallMsg) (u
 	return gasLimit, nil
 }
 
-func FilterLogsWithRetry(client *ethclient.Client, query ethereum.FilterQuery) ([]types.Log, error) {
+func FilterLogsWithRetry(client *ethclient.Client, query ethereum.FilterQuery, razorUtils RazorUtilsInterface) ([]types.Log, error) {
 	var (
 		logs []types.Log
 		err  error
 	)
 	err = retry.Do(
 		func() error {
-			logs, err = client.FilterLogs(context.Background(), query)
+			logs, err = razorUtils.FilterLogs(client, context.Background(), query)
 			if err != nil {
 				log.Error("Error in fetching logs.... Retrying")
 				return err
 			}
 			return nil
-		},
-	)
+		}, razorUtils.RetryAttempts(core.MaxRetries))
 	if err != nil {
 		return nil, err
 	}
 	return logs, nil
 }
 
-func BalanceAtWithRetry(client *ethclient.Client, account common.Address) (*big.Int, error) {
+func BalanceAtWithRetry(client *ethclient.Client, account common.Address, razorUtils RazorUtilsInterface) (*big.Int, error) {
 	var (
 		balance *big.Int
 		err     error
 	)
 	err = retry.Do(
 		func() error {
-			balance, err = client.BalanceAt(context.Background(), account, nil)
+			balance, err = razorUtils.BalanceAt(client, context.Background(), account, nil)
 			if err != nil {
 				log.Error("Error in fetching logs.... Retrying")
 				return err
 			}
 			return nil
-		},
-	)
+		}, razorUtils.RetryAttempts(core.MaxRetries))
 	if err != nil {
 		return nil, err
 	}
