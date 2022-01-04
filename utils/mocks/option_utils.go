bd32ab4c0aeea2c07266b6d79a9bdb5f0d719bba
Yashk767 <yash.kurlekar2017@gmail.com>
2022-01-04 15:22:04 +0530

    Added mocks

--- utils/mocks/option_utils.go
+++ utils/mocks/option_utils.go
@@ -11,6 +11,8 @@ import (
 
 	bind "github.com/ethereum/go-ethereum/accounts/abi/bind"
 
+	bindings "razor/pkg/bindings"
+
 	common "github.com/ethereum/go-ethereum/common"
 
 	context "context"
@@ -102,6 +104,27 @@ func (_m *OptionUtils) FilterLogs(_a0 *ethclient.Client, _a1 context.Context, _a
 	return r0, r1
 }
 
+// GetBlock provides a mock function with given fields: _a0, _a1, _a2
+func (_m *OptionUtils) GetBlock(_a0 *ethclient.Client, _a1 *bind.CallOpts, _a2 uint32) (bindings.StructsBlock, error) {
+	ret := _m.Called(_a0, _a1, _a2)
+
+	var r0 bindings.StructsBlock
+	if rf, ok := ret.Get(0).(func(*ethclient.Client, *bind.CallOpts, uint32) bindings.StructsBlock); ok {
+		r0 = rf(_a0, _a1, _a2)
+	} else {
+		r0 = ret.Get(0).(bindings.StructsBlock)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*ethclient.Client, *bind.CallOpts, uint32) error); ok {
+		r1 = rf(_a0, _a1, _a2)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetDefaultPath provides a mock function with given fields:
 func (_m *OptionUtils) GetDefaultPath() (string, error) {
 	ret := _m.Called()
@@ -116,6 +139,27 @@ func (_m *OptionUtils) GetDefaultPath() (string, error) {
 	var r1 error
 	if rf, ok := ret.Get(1).(func() error); ok {
 		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// GetNumProposedBlocks provides a mock function with given fields: _a0, _a1, _a2
+func (_m *OptionUtils) GetNumProposedBlocks(_a0 *ethclient.Client, _a1 *bind.CallOpts, _a2 uint32) (uint8, error) {
+	ret := _m.Called(_a0, _a1, _a2)
+
+	var r0 uint8
+	if rf, ok := ret.Get(0).(func(*ethclient.Client, *bind.CallOpts, uint32) uint8); ok {
+		r0 = rf(_a0, _a1, _a2)
+	} else {
+		r0 = ret.Get(0).(uint8)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*ethclient.Client, *bind.CallOpts, uint32) error); ok {
+		r1 = rf(_a0, _a1, _a2)
 	} else {
 		r1 = ret.Error(1)
 	}
@@ -139,6 +183,27 @@ func (_m *OptionUtils) GetPrivateKey(address string, password string, keystorePa
 	return r0
 }
 
+// GetProposedBlock provides a mock function with given fields: _a0, _a1, _a2, _a3
+func (_m *OptionUtils) GetProposedBlock(_a0 *ethclient.Client, _a1 *bind.CallOpts, _a2 uint32, _a3 uint32) (bindings.StructsBlock, error) {
+	ret := _m.Called(_a0, _a1, _a2, _a3)
+
+	var r0 bindings.StructsBlock
+	if rf, ok := ret.Get(0).(func(*ethclient.Client, *bind.CallOpts, uint32, uint32) bindings.StructsBlock); ok {
+		r0 = rf(_a0, _a1, _a2, _a3)
+	} else {
+		r0 = ret.Get(0).(bindings.StructsBlock)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*ethclient.Client, *bind.CallOpts, uint32, uint32) error); ok {
+		r1 = rf(_a0, _a1, _a2, _a3)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // HeaderByNumber provides a mock function with given fields: _a0, _a1, _a2
 func (_m *OptionUtils) HeaderByNumber(_a0 *ethclient.Client, _a1 context.Context, _a2 *big.Int) (*types.Header, error) {
 	ret := _m.Called(_a0, _a1, _a2)
@@ -155,6 +220,50 @@ func (_m *OptionUtils) HeaderByNumber(_a0 *ethclient.Client, _a1 context.Context
 	var r1 error
 	if rf, ok := ret.Get(1).(func(*ethclient.Client, context.Context, *big.Int) error); ok {
 		r1 = rf(_a0, _a1, _a2)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// MaxAltBlocks provides a mock function with given fields: _a0, _a1
+func (_m *OptionUtils) MaxAltBlocks(_a0 *ethclient.Client, _a1 *bind.CallOpts) (uint8, error) {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 uint8
+	if rf, ok := ret.Get(0).(func(*ethclient.Client, *bind.CallOpts) uint8); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		r0 = ret.Get(0).(uint8)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*ethclient.Client, *bind.CallOpts) error); ok {
+		r1 = rf(_a0, _a1)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// MinStake provides a mock function with given fields: _a0, _a1
+func (_m *OptionUtils) MinStake(_a0 *ethclient.Client, _a1 *bind.CallOpts) (*big.Int, error) {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 *big.Int
+	if rf, ok := ret.Get(0).(func(*ethclient.Client, *bind.CallOpts) *big.Int); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*big.Int)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*ethclient.Client, *bind.CallOpts) error); ok {
+		r1 = rf(_a0, _a1)
 	} else {
 		r1 = ret.Error(1)
 	}
@@ -267,6 +376,27 @@ func (_m *OptionUtils) RetryAttempts(_a0 uint) retry.Option {
 	}
 
 	return r0
+}
+
+// SortedProposedBlockIds provides a mock function with given fields: _a0, _a1, _a2, _a3
+func (_m *OptionUtils) SortedProposedBlockIds(_a0 *ethclient.Client, _a1 *bind.CallOpts, _a2 uint32, _a3 *big.Int) (uint32, error) {
+	ret := _m.Called(_a0, _a1, _a2, _a3)
+
+	var r0 uint32
+	if rf, ok := ret.Get(0).(func(*ethclient.Client, *bind.CallOpts, uint32, *big.Int) uint32); ok {
+		r0 = rf(_a0, _a1, _a2, _a3)
+	} else {
+		r0 = ret.Get(0).(uint32)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*ethclient.Client, *bind.CallOpts, uint32, *big.Int) error); ok {
+		r1 = rf(_a0, _a1, _a2, _a3)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
 }
 
 // SuggestGasPrice provides a mock function with given fields: _a0, _a1
