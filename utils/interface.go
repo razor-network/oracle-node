b16e38755857440d9a2986c2966738ab8f135e3b
Yashk767 <yash.kurlekar2017@gmail.com>
2022-01-04 15:21:19 +0530

    Added method caller UtilsStruct for block.go functions

--- utils/interface.go
+++ utils/interface.go
@@ -14,6 +14,7 @@ import (
 	"math/big"
 	"razor/accounts"
 	"razor/core/types"
+	"razor/pkg/bindings"
 )
 
 //go:generate mockery --name OptionUtils --output ./mocks/ --case=underscore
@@ -35,6 +36,12 @@ type OptionUtils interface {
 	EstimateGas(*ethclient.Client, context.Context, ethereum.CallMsg) (uint64, error)
 	FilterLogs(*ethclient.Client, context.Context, ethereum.FilterQuery) ([]Types.Log, error)
 	BalanceAt(*ethclient.Client, context.Context, common.Address, *big.Int) (*big.Int, error)
+	GetNumProposedBlocks(*ethclient.Client, *bind.CallOpts, uint32) (uint8, error)
+	GetProposedBlock(*ethclient.Client, *bind.CallOpts, uint32, uint32) (bindings.StructsBlock, error)
+	GetBlock(*ethclient.Client, *bind.CallOpts, uint32) (bindings.StructsBlock, error)
+	MinStake(*ethclient.Client, *bind.CallOpts) (*big.Int, error)
+	MaxAltBlocks(*ethclient.Client, *bind.CallOpts) (uint8, error)
+	SortedProposedBlockIds(*ethclient.Client, *bind.CallOpts, uint32, *big.Int) (uint32, error)
 }
 
 type Utils interface {
@@ -43,20 +50,25 @@ type Utils interface {
 	GetPendingNonceAtWithRetry(*ethclient.Client, common.Address) (uint64, error)
 	GetGasPrice(*ethclient.Client, types.Configurations) *big.Int
 	GetTxnOpts(types.TransactionOptions) *bind.TransactOpts
-	GetGasLimit(transactionData types.TransactionOptions, txnOpts *bind.TransactOpts) (uint64, error)
-	EstimateGasWithRetry(client *ethclient.Client, message ethereum.CallMsg) (uint64, error)
-	IncreaseGasLimitValue(client *ethclient.Client, gasLimit uint64, gasLimitMultiplier float32) (uint64, error)
-	GetLatestBlockWithRetry(client *ethclient.Client) (*Types.Header, error)
-	FilterLogsWithRetry(client *ethclient.Client, query ethereum.FilterQuery) ([]Types.Log, error)
-	BalanceAtWithRetry(client *ethclient.Client, account common.Address) (*big.Int, error)
+	GetGasLimit(types.TransactionOptions, *bind.TransactOpts) (uint64, error)
+	EstimateGasWithRetry(*ethclient.Client, ethereum.CallMsg) (uint64, error)
+	IncreaseGasLimitValue(*ethclient.Client, uint64, float32) (uint64, error)
+	GetLatestBlockWithRetry(*ethclient.Client) (*Types.Header, error)
+	FilterLogsWithRetry(*ethclient.Client, ethereum.FilterQuery) ([]Types.Log, error)
+	BalanceAtWithRetry(*ethclient.Client, common.Address) (*big.Int, error)
+	GetBlockManager(*ethclient.Client) *bindings.BlockManager
+	GetOptions() bind.CallOpts
+	GetNumberOfProposedBlocks(*ethclient.Client, uint32) (uint8, error)
+	GetSortedProposedBlockId(*ethclient.Client, uint32, *big.Int) (uint32, error)
+	FetchPreviousValue(*ethclient.Client, uint32, uint16) (uint32, error)
+	GetMaxAltBlocks(*ethclient.Client) (uint8, error)
+	GetMinStakeAmount(*ethclient.Client) (*big.Int, error)
+	GetProposedBlock(*ethclient.Client, uint32, uint32) (bindings.StructsBlock, error)
+	GetSortedProposedBlockIds(*ethclient.Client, uint32) ([]uint32, error)
+	GetBlockManagerWithOpts(*ethclient.Client) (*bindings.BlockManager, bind.CallOpts)
 }
 
 type OptionsStruct struct{}
-type ABIUtilsStruct struct{}
-type PathStruct struct{}
-type AccountStruct struct{}
-type BindStruct struct{}
-
 type UtilsStruct struct{}
 
 type OptionsPackageStruct struct {
