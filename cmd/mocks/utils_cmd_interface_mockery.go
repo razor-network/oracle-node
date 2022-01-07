8dd0fced08754dc404e5625175ae4e7da6991bb6
Yashk767 <yash.kurlekar2017@gmail.com>
2022-01-07 13:52:58 +0530

    Added mocks

--- cmd/mocks/utils_cmd_interface_mockery.go
+++ cmd/mocks/utils_cmd_interface_mockery.go
@@ -117,6 +117,29 @@ func (_m *UtilsCmdInterfaceMockery) Commit(_a0 *ethclient.Client, _a1 []*big.Int
 	return r0, r1
 }
 
+// CreateCollection provides a mock function with given fields: _a0, _a1, _a2
+func (_m *UtilsCmdInterfaceMockery) CreateCollection(_a0 *ethclient.Client, _a1 types.Configurations, _a2 types.CreateCollectionInput) (common.Hash, error) {
+	ret := _m.Called(_a0, _a1, _a2)
+
+	var r0 common.Hash
+	if rf, ok := ret.Get(0).(func(*ethclient.Client, types.Configurations, types.CreateCollectionInput) common.Hash); ok {
+		r0 = rf(_a0, _a1, _a2)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(common.Hash)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*ethclient.Client, types.Configurations, types.CreateCollectionInput) error); ok {
+		r1 = rf(_a0, _a1, _a2)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // CreateJob provides a mock function with given fields: _a0, _a1, _a2
 func (_m *UtilsCmdInterfaceMockery) CreateJob(_a0 *ethclient.Client, _a1 types.Configurations, _a2 types.CreateJobInput) (common.Hash, error) {
 	ret := _m.Called(_a0, _a1, _a2)
@@ -142,6 +165,11 @@ func (_m *UtilsCmdInterfaceMockery) CreateJob(_a0 *ethclient.Client, _a1 types.C
 
 // ExecuteClaimBounty provides a mock function with given fields: _a0
 func (_m *UtilsCmdInterfaceMockery) ExecuteClaimBounty(_a0 *pflag.FlagSet) {
+	_m.Called(_a0)
+}
+
+// ExecuteCreateCollection provides a mock function with given fields: _a0
+func (_m *UtilsCmdInterfaceMockery) ExecuteCreateCollection(_a0 *pflag.FlagSet) {
 	_m.Called(_a0)
 }
 
@@ -195,6 +223,34 @@ func (_m *UtilsCmdInterfaceMockery) GetConfigData() (types.Configurations, error
 	}
 
 	return r0, r1
+}
+
+// GetEpochAndState provides a mock function with given fields: _a0
+func (_m *UtilsCmdInterfaceMockery) GetEpochAndState(_a0 *ethclient.Client) (uint32, int64, error) {
+	ret := _m.Called(_a0)
+
+	var r0 uint32
+	if rf, ok := ret.Get(0).(func(*ethclient.Client) uint32); ok {
+		r0 = rf(_a0)
+	} else {
+		r0 = ret.Get(0).(uint32)
+	}
+
+	var r1 int64
+	if rf, ok := ret.Get(1).(func(*ethclient.Client) int64); ok {
+		r1 = rf(_a0)
+	} else {
+		r1 = ret.Get(1).(int64)
+	}
+
+	var r2 error
+	if rf, ok := ret.Get(2).(func(*ethclient.Client) error); ok {
+		r2 = rf(_a0)
+	} else {
+		r2 = ret.Error(2)
+	}
+
+	return r0, r1, r2
 }
 
 // GetGasLimit provides a mock function with given fields:
@@ -436,6 +492,34 @@ func (_m *UtilsCmdInterfaceMockery) Transfer(_a0 *ethclient.Client, _a1 types.Co
 	var r1 error
 	if rf, ok := ret.Get(1).(func(*ethclient.Client, types.Configurations, types.TransferInput) error); ok {
 		r1 = rf(_a0, _a1, _a2)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// WaitForAppropriateState provides a mock function with given fields: _a0, _a1, _a2
+func (_m *UtilsCmdInterfaceMockery) WaitForAppropriateState(_a0 *ethclient.Client, _a1 string, _a2 ...int) (uint32, error) {
+	_va := make([]interface{}, len(_a2))
+	for _i := range _a2 {
+		_va[_i] = _a2[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, _a0, _a1)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 uint32
+	if rf, ok := ret.Get(0).(func(*ethclient.Client, string, ...int) uint32); ok {
+		r0 = rf(_a0, _a1, _a2...)
+	} else {
+		r0 = ret.Get(0).(uint32)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*ethclient.Client, string, ...int) error); ok {
+		r1 = rf(_a0, _a1, _a2...)
 	} else {
 		r1 = ret.Error(1)
 	}
