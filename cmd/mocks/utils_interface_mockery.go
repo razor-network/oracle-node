d02c62d301cc4d77f42c399e4af8faffdb959dd2
Yashk767 <yash.kurlekar2017@gmail.com>
2022-01-12 12:34:54 +0530

    Added mocks

--- cmd/mocks/utils_interface_mockery.go
+++ cmd/mocks/utils_interface_mockery.go
@@ -1012,6 +1012,27 @@ func (_m *UtilsInterfaceMockery) IsFlagPassed(_a0 string) bool {
 	return r0
 }
 
+// ParseBool provides a mock function with given fields: str
+func (_m *UtilsInterfaceMockery) ParseBool(str string) (bool, error) {
+	ret := _m.Called(str)
+
+	var r0 bool
+	if rf, ok := ret.Get(0).(func(string) bool); ok {
+		r0 = rf(str)
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string) error); ok {
+		r1 = rf(str)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // Sleep provides a mock function with given fields: _a0
 func (_m *UtilsInterfaceMockery) Sleep(_a0 time.Duration) {
 	_m.Called(_a0)
