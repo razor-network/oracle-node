85fc96eac88544954e80102944e7c84c560d5f94
Yashk767 <yash.kurlekar2017@gmail.com>
2022-01-21 13:11:52 +0530

    Added check that ony proposer can claim block reward

--- cmd/confirm.go
+++ cmd/confirm.go
@@ -10,13 +10,42 @@ import (
 var blockManagerUtils blockManagerInterface
 
 func (utilsStruct UtilsStruct) ClaimBlockReward(options types.TransactionOptions) (common.Hash, error) {
-	log.Info("Claiming block reward...")
-	txnOpts := utilsStruct.razorUtils.GetTxnOpts(options)
-	txn, err := utilsStruct.blockManagerUtils.ClaimBlockReward(options.Client, txnOpts)
+	epoch, err := utilsStruct.razorUtils.GetEpoch(options.Client)
 	if err != nil {
-		log.Error("Error in claiming block reward: ", err)
+		log.Error("Error in getting epoch: ", err)
 		return core.NilHash, err
 	}
-	log.Info("Txn Hash: ", utilsStruct.transactionUtils.Hash(txn).Hex())
-	return utilsStruct.transactionUtils.Hash(txn), nil
+
+	sortedProposedBlockIds, err := utilsStruct.razorUtils.GetSortedProposedBlockIds(options.Client, options.AccountAddress, epoch)
+	if err != nil {
+		log.Error("Error in getting sortedProposedBlockIds: ", err)
+		return core.NilHash, err
+	}
+
+	stakerID, err := utilsStruct.razorUtils.GetStakerId(options.Client, options.AccountAddress)
+	if err != nil {
+		log.Error("Error in getting stakerId: ", err)
+		return core.NilHash, err
+	}
+
+	selectedProposedBlock, err := utilsStruct.razorUtils.GetProposedBlock(options.Client, options.AccountAddress, epoch, sortedProposedBlockIds[0])
+	if err != nil {
+		log.Error("Error in getting selectedProposedBlock: ", err)
+		return core.NilHash, err
+	}
+
+	if selectedProposedBlock.ProposerId == stakerID {
+		log.Info("Claiming block reward...")
+		txnOpts := utilsStruct.razorUtils.GetTxnOpts(options)
+		txn, err := utilsStruct.blockManagerUtils.ClaimBlockReward(options.Client, txnOpts)
+		if err != nil {
+			log.Error("Error in claiming block reward: ", err)
+			return core.NilHash, err
+		}
+		log.Info("Txn Hash: ", utilsStruct.transactionUtils.Hash(txn).Hex())
+		return utilsStruct.transactionUtils.Hash(txn), nil
+	}
+
+	log.Debug("Only selected block proposer can claim block reward")
+	return core.NilHash, nil
 }
