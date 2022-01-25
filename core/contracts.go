9fda949ee8ec705da9d16607f76e1d132598a3c2
Shrikant Upadhyay <shrikant1997upadhyay@gmail.com>
2022-01-25 17:10:32 +0530

    v0.2.0-incentnet-staging-address

--- core/contracts.go
+++ core/contracts.go
@@ -1,7 +1,7 @@
 package core
 
-var StakeManagerAddress = "0x7C688A506B28B0E5ACa810aDac758504AAC3D9F9"
-var RAZORAddress = "0xf48A4226e852d0879774964A859e6cca7A6820A9"
-var AssetManagerAddress = "0x7fFc58c4e5E8bf37cec1698b988f5BaF0A780Ed7"
-var VoteManagerAddress = "0xfd95647b776d44e3415bfbb2C145c951dcF11dB1"
-var BlockManagerAddress = "0x3fb53573545D8F5c3F64bC5EA7a7d76FcdA0A492"
+var StakeManagerAddress = "0x0cB7baFA14B32353463171f6cFB6c74479E7DFC7"
+var RAZORAddress = "0x7808736427ee91d86db57E395FC463fe45Ca0d0C"
+var AssetManagerAddress = "0x5d0DF185C0510FCe81412f9a881fBDe7F1C06753"
+var VoteManagerAddress = "0x9C7720D9e18f0ae4d36bc09a830e209e1570043E"
+var BlockManagerAddress = "0x39c3611543A50789EBc13d95C72c167fc38a8a40"
