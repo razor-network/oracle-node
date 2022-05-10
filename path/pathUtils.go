e481b11af7fbe4dd181157f79faf44a8f64e1e80
Shekhar Gaur <shekhargaur1999@gmail.com>
2022-05-10 13:40:10 +0530

    comments added for godoc for path

--- path/pathUtils.go
+++ path/pathUtils.go
@@ -1,3 +1,4 @@
+//Package path provides all path related functions
 package path
 
 import (
@@ -33,26 +34,32 @@ type OSInterface interface {
 type PathUtils struct{}
 type OSUtils struct{}
 
+//This function returns the home directory of user
 func (o OSUtils) UserHomeDir() (string, error) {
 	return os.UserHomeDir()
 }
 
+//This function is used to get status of the specified path
 func (o OSUtils) Stat(name string) (fs.FileInfo, error) {
 	return os.Stat(name)
 }
 
+//This function returns the bool which shows the particular file exists or not
 func (o OSUtils) IsNotExist(err error) bool {
 	return os.IsNotExist(err)
 }
 
+//This function is used to make a new directory
 func (o OSUtils) Mkdir(name string, perm fs.FileMode) error {
 	return os.Mkdir(name, perm)
 }
 
+//This function is used to open the file and this is generalized open call
 func (o OSUtils) OpenFile(name string, flag int, perm fs.FileMode) (*os.File, error) {
 	return os.OpenFile(name, flag, perm)
 }
 
+//This function is used to open the file
 func (o OSUtils) Open(name string) (*os.File, error) {
 	return os.Open(name)
 }
