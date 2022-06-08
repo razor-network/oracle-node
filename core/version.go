f6ece4e540082340e119b0a6ab2bdde3caf6dcfb
Yashk767 <76935991+Yashk767@users.noreply.github.com>
2022-06-08 16:19:37 +0530

    hotfix version update (#748)
    * version updated
    
    * added stable tag

--- core/version.go
+++ core/version.go
@@ -3,10 +3,10 @@ package core
 import "fmt"
 
 const (
-	VersionMajor = 1        // Major version component of the current release
-	VersionMinor = 0        // Minor version component of the current release
-	VersionPatch = 2        // Patch version component of the current release
-	VersionMeta  = "stable" // Version metadata to append to the version string
+	VersionMajor = 1                                           // Major version component of the current release
+	VersionMinor = 0                                           // Minor version component of the current release
+	VersionPatch = 3                                           // Patch version component of the current release
+	VersionMeta  = "incentivised-testnet-phase2-patch3-stable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
