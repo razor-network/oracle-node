abc8629ce23f27ae4c76fd195fea6452f4081eb2
Shekhar Gaur <shekhargaur1999@gmail.com>
2022-06-20 22:42:37 +0530

    test added for strongPassword function

--- utils/password_test.go
+++ utils/password_test.go
@@ -11,7 +11,55 @@ func Test_strongPassword(t *testing.T) {
 		args args
 		want bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "When strongPassword() returns true and every condition is met",
+			args: args{
+				input: "Qwerty12@",
+			},
+			want: true,
+		},
+		{
+			name: "When strongPassword() returns false",
+			args: args{
+				input: "test",
+			},
+			want: false,
+		},
+		{
+			name: "When password does not contain uppercase letters",
+			args: args{
+				input: "qwerty12@",
+			},
+			want: false,
+		},
+		{
+			name: "When password does not contain lowercase letters",
+			args: args{
+				input: "QWERTY12@",
+			},
+			want: false,
+		},
+		{
+			name: "When password does not contain digits",
+			args: args{
+				input: "qwerty!#%@",
+			},
+			want: false,
+		},
+		{
+			name: "When password does not contain special character",
+			args: args{
+				input: "qwerty1234",
+			},
+			want: false,
+		},
+		{
+			name: "When password is not long enough",
+			args: args{
+				input: "Qw1@",
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
