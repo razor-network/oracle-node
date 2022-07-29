842332297e359cce71ff834559ee62325b94a034
Handschrift <privacy.mtehw@aleeas.com>
2022-07-29 06:18:04 +0000

    replaced path.join with filepath.join (#896)

--- cmd/create.go
+++ cmd/create.go
@@ -6,7 +6,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
-	"path"
+	"path/filepath"
 	razorAccounts "razor/accounts"
 	"razor/utils"
 )
@@ -44,7 +44,7 @@ func (*UtilsStruct) Create(password string) (accounts.Account, error) {
 		log.Error("Error in fetching .razor directory")
 		return accounts.Account{Address: common.Address{0x00}}, err
 	}
-	keystorePath := path.Join(razorPath, "keystore_files")
+	keystorePath := filepath.Join(razorPath, "keystore_files")
 	account := razorAccounts.AccountUtilsInterface.CreateAccount(keystorePath, password)
 	return account, nil
 }
