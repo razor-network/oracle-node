b55240acfb51f322434745b83b82bb385a4137c2
Yashk767 <76935991+Yashk767@users.noreply.github.com>
2022-10-07 16:53:36 +0530

    Updated `v1.3.0` branch to staging parameters for v1.3.0-alpha version (#967)
    * Updated contract addresses and chainId to staging
    
    * Updated razor-go version to v1.3.0-alpha

--- core/constants.go
+++ core/constants.go
@@ -7,7 +7,7 @@ import (
 
 var EpochLength int64 = 1200
 var NumberOfStates int64 = 5
-var ChainId = big.NewInt(0x109B4597)
+var ChainId = big.NewInt(132333505628089)
 var StateLength = uint64(EpochLength / NumberOfStates)
 var MaxRetries uint = 8
 var NilHash = common.Hash{0x00}
