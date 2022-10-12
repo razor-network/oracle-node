927507e8cfc0ae7cd3ec5159fcdffdf419e1258d
YashK <yash.kurlekar2017@gmail.com>
2022-10-12 15:16:27 +0530

    Added default values for config parameters

--- core/constants.go
+++ core/constants.go
@@ -12,3 +12,12 @@ var StateLength = uint64(EpochLength / NumberOfStates)
 var MaxRetries uint = 8
 var NilHash = common.Hash{0x00}
 var BlockCompletionTimeout = 30
+
+var DefaultProvider = "http://127.0.0.1:8545"
+var DefaultGasMultiplier = 1.0
+var DefaultBufferPercent = 20
+var DefaultGasPrice = 1
+var DefaultWaitTime = 1
+var DefaultGasLimit = 2
+var DefaultRPCTimeout = 10
+var DefaultLogLevel = ""
