cb7645a1ef0eb2e93bf73e4f5507c3216c6446e9
YashK <yash.kurlekar2017@gmail.com>
2023-08-21 22:48:44 -0500

    Removed default alternate provider from set config script

--- config.sh
+++ config.sh
@@ -11,11 +11,7 @@ then
   PROVIDER="http://127.0.0.1:8545"
 fi
 
-read -rp "Alternate Provider: (http://127.0.0.1:8545) " ALTERNATE_PROVIDER
-if [ -z "$ALTERNATE_PROVIDER" ];
-then
-  ALTERNATE_PROVIDER="http://127.0.0.1:8545"
-fi
+read -rp "Alternate Provider: " ALTERNATE_PROVIDER
 
 read -rp "Gas Multiplier: (1.0) " GAS_MULTIPLIER
 if [ -z "$GAS_MULTIPLIER" ];
@@ -58,4 +54,10 @@ read -rp "Log Files Max Age: (365) " MAX_AGE
 if [ -z "$MAX_AGE" ]; then
    MAX_AGE=365
 fi
-$RAZOR setConfig -p $PROVIDER --alternateProvider $ALTERNATE_PROVIDER -b $BUFFER -g $GAS_MULTIPLIER -w $WAIT_TIME --gasprice $GAS_PRICE --gasLimit $GAS_LIMIT --rpcTimeout 10 --httpTimeout 10 --logFileMaxSize $MAX_SIZE --logFileMaxBackups $MAX_BACKUPS --logFileMaxAge $MAX_AGE
+
+if [ -z "$ALTERNATE_PROVIDER" ]; then
+  $RAZOR setConfig -p $PROVIDER -b $BUFFER -g $GAS_MULTIPLIER -w $WAIT_TIME --gasprice $GAS_PRICE --gasLimit $GAS_LIMIT --rpcTimeout 10 --httpTimeout 10 --logFileMaxSize $MAX_SIZE --logFileMaxBackups $MAX_BACKUPS --logFileMaxAge $MAX_AGE
+else
+$RAZOR setConfig -p $PROVIDER --alternateProvider "$ALTERNATE_PROVIDER" -b $BUFFER -g $GAS_MULTIPLIER -w $WAIT_TIME --gasprice $GAS_PRICE --gasLimit $GAS_LIMIT --rpcTimeout 10 --httpTimeout 10 --logFileMaxSize $MAX_SIZE --logFileMaxBackups $MAX_BACKUPS --logFileMaxAge $MAX_AGE
+
+fi
