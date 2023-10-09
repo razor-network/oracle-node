b1a71babec6dfeefc51851bfa9766b6dbc6d300b
Yashk767 <76935991+Yashk767@users.noreply.github.com>
2023-10-09 06:54:04 -0500

    Update the deployment parameters to mainnet for `v1.0.7-beta` (#1141)
    * Updated deployement parameters
    
    * Updated version to v1.0.7-beta

--- core/contracts.go
+++ core/contracts.go
@@ -1,7 +1,7 @@
 package core
 
-var StakeManagerAddress = "0x9f55a2C6C1F1Be8B01562cEae2df2F22931C7a46"
-var RAZORAddress = "0x4500E10fEb89e46E9fb642D0c62b1a761278155D"
-var CollectionManagerAddress = "0x3b76eB8c0282dAf531D7C507E4f3143A9A9c38b1"
-var VoteManagerAddress = "0x11995b74D6d07a6Edc05653a71F3e8B3354caBF0"
-var BlockManagerAddress = "0x096e44B0d8b68376C8Efe40F28C3857951f03069"
+var StakeManagerAddress = "0xe0bC695203d9C9f379bcdE9260B9F71B64B85298"
+var RAZORAddress = "0xcbf70914Fae03B3acB91E953De60CfDAaCA8145f"
+var CollectionManagerAddress = "0x367962d1462C568A0dDd0e2448311469451bF5a3"
+var VoteManagerAddress = "0x641BAD0641eB5B94B19568C0a22a55AEbDAF1870"
+var BlockManagerAddress = "0x11aB70d78f1Dd2c3F967180d8A64858Db03A0aBa"
