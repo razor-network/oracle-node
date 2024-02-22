0df52aaa9f64cb94c7340d28743c5c3a45e35d33
YashK <yash.kurlekar2017@gmail.com>
2024-02-23 01:02:44 +0530

    refactor: moved deployement parameters to network_details

--- core/network_details.go
+++ core/network_details.go
@@ -5,3 +5,6 @@ var RAZORAddress = "0xbF3BCf61f64C9825C7Af3f3D79817c9Cf44afBa7"
 var CollectionManagerAddress = "0x7B8BF9a0B648Dd9f75D5E9e8a3054D18E8C4Cc5e"
 var VoteManagerAddress = "0x86886e048fd0f253E6E75c13a3f924598799e06A"
 var BlockManagerAddress = "0x39D4B78bb09DEEC54fA2df729808b19962Cb9aae"
+
+var CHAIN_NAME =
+var CHAIN_ID =
