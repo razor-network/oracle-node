c0ff52cd27da75d5b9bac70e1a69e8180f4c7efc
YashK <yash.kurlekar2017@gmail.com>
2024-03-27 13:26:12 +0530

    revert: optimised fetching key from keystore dir

--- accounts/accountUtils.go
+++ accounts/accountUtils.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/crypto"
+	"os"
 	"razor/core/types"
 )
 
@@ -15,13 +16,23 @@ var AccountUtilsInterface AccountInterface
 
 type AccountInterface interface {
 	CreateAccount(path string, password string) accounts.Account
+	GetPrivateKeyFromKeystore(keystorePath string, password string) (*ecdsa.PrivateKey, error)
 	GetPrivateKey(address string, password string, keystorePath string) (*ecdsa.PrivateKey, error)
 	SignData(hash []byte, account types.Account, defaultPath string) ([]byte, error)
+	Accounts(path string) []accounts.Account
 	NewAccount(path string, passphrase string) (accounts.Account, error)
+	DecryptKey(jsonBytes []byte, password string) (*keystore.Key, error)
 	Sign(digestHash []byte, prv *ecdsa.PrivateKey) ([]byte, error)
+	ReadFile(filename string) ([]byte, error)
 }
 
 type AccountUtils struct{}
+
+//This function returns all the accounts in form of array
+func (accountUtils AccountUtils) Accounts(path string) []accounts.Account {
+	ks := keystore.NewKeyStore(path, keystore.StandardScryptN, keystore.StandardScryptP)
+	return ks.Accounts()
+}
 
 //This function takes path and pass phrase as input and returns the new account
 func (accountUtils AccountUtils) NewAccount(path string, passphrase string) (accounts.Account, error) {
@@ -30,7 +41,17 @@ func (accountUtils AccountUtils) NewAccount(path string, passphrase string) (acc
 	return ks.NewAccount(passphrase)
 }
 
+//This function takes json bytes array and password as input and returns the decrypted key
+func (accountUtils AccountUtils) DecryptKey(jsonBytes []byte, password string) (*keystore.Key, error) {
+	return keystore.DecryptKey(jsonBytes, password)
+}
+
 //This function takes hash in form of byte array and private key as input and returns signature as byte array
 func (accountUtils AccountUtils) Sign(digestHash []byte, prv *ecdsa.PrivateKey) (sig []byte, err error) {
 	return crypto.Sign(digestHash, prv)
+}
+
+//This function takes name of the file as input and returns the file data as byte array
+func (accountUtils AccountUtils) ReadFile(filename string) ([]byte, error) {
+	return os.ReadFile(filename)
 }
